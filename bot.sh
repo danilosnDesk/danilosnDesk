fc48f24ac8099c3b3e8523601fa0fddcf7fa76e5
Danilson Kayumbuca <artmiss264@gmail.com>
2024-03-13 17:42:16 +0100

    Commit 1

--- bot.sh
+++ bot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Loop para realizar 100 commits
-for i in {1..500}
+for i in {1..5}
 do
     echo "Commit $i"
     echo "$i" >> README.md  
     git add .
     git commit -m "Commit $i"
+    git push
 done
